42ec8d2710763b6d45d3e78d2b76213a3325f7dc
Teboho Mosakoa <mosakoateboho@gmail.com>
2020-07-07 00:10:01 +0200

    Add project files.

---
+++ RegistrationPage/Areas/Identity/Data/RegistrationPageUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace RegistrationPage.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the RegistrationPageUser class
+    public class RegistrationPageUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
---
+++ RegistrationPage/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RegistrationPage.Areas.Identity.Data;
+using RegistrationPage.Models;
+
+[assembly: HostingStartup(typeof(RegistrationPage.Areas.Identity.IdentityHostingStartup))]
+namespace RegistrationPage.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<RegistrationPageDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("RegistrationPageDbContextConnection")));
+
+                services.AddDefaultIdentity<RegistrationPageUser>(options => {
+                        options.SignIn.RequireConfirmedAccount = false;
+                        options.User.RequireUniqueEmail = true;
+                    })
+                    .AddEntityFrameworkStores<RegistrationPageDbContext>();
+            });
+        }
+    }
+}
