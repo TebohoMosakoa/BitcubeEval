a3e4588f3e93fb3269634b597d74582062f1ca93
Teboho Mosakoa <mosakoateboho@gmail.com>
2020-07-09 12:33:30 +0200

    Section 3

--- RegistrationPage/Controllers/HomeController.cs
+++ RegistrationPage/Controllers/HomeController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RegistrationPage.Areas.Identity.Data;
 using RegistrationPage.Models;
 
 namespace RegistrationPage.Controllers
@@ -12,15 +15,28 @@ namespace RegistrationPage.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<RegistrationPageUser> _userManager;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<RegistrationPageUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
-
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Profile()
+        {
+            var userId = _userManager.GetUserId(HttpContext.User);
+            RegistrationPageUser user = _userManager.FindByIdAsync(userId).Result;
+            return View(user);
+        }
+        public IActionResult Friends()
+        {
+            var users = _userManager.Users; 
+            return View(users);
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
